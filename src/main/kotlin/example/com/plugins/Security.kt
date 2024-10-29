25c3334979feff4c0b1eb2d5108cf4adfa48df80
Artur <jagkota@gmail.com>
2024-10-29 11:03:06 +0300

    add security route

--- src/main/kotlin/example/com/plugins/Security.kt
+++ src/main/kotlin/example/com/plugins/Security.kt
@@ -13,14 +13,33 @@ fun Application.configureSecurity() {
             // pass="test", HA1=MD5("test:MyRealm:pass")="fb12475e62dedc5c2744d98eb73b8877"
             "test" to hex("fb12475e62dedc5c2744d98eb73b8877")
         )
-    
+
         digest("myDigestAuth") {
             digestProvider { userName, realm ->
                 usersInMyRealmToHA1[userName]
             }
         }
+
+        basic(name = "myauth1") {
+            realm = "Ktor Server"
+            validate { credentials ->
+                if (credentials.name == credentials.password) {
+                    UserIdPrincipal(credentials.name)
+                } else {
+                    null
+                }
+            }
+
+            form { }
+        }
     }
     routing {
+        authenticate("myauth1") {
+            get("/protected/route/basic") {
+                val principal = call.principal<UserIdPrincipal>()!!
+                call.respondText("Hello ${principal.name}")
+            }
+        }
         authenticate("myDigestAuth") {
             get("/protected/route/digest") {
                 val principal = call.principal<UserIdPrincipal>()!!
