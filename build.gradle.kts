9f00cca1b63b768db90b1b9bea9cc4aa15645ee8
Artur <jagkota@gmail.com>
2024-10-29 10:29:27 +0300

    add status pages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
@@ -30,6 +31,8 @@ dependencies {
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
 }
 
 tasks {
--- src/main/kotlin/example/com/plugins/Routing.kt
+++ src/main/kotlin/example/com/plugins/Routing.kt
@@ -1,10 +1,20 @@
 package example.com.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    install(StatusPages) {
+        status(HttpStatusCode.NotFound) { call, _ ->
+            call.respondText(text = "Not Found", status = HttpStatusCode.NotFound)
+        }
+        exception<Throwable> { call, cause ->
+            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
+        }
+    }
     routing {
         get("/") {
             call.respondText("Hello World!")
